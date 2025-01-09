db121540afb50a5ee639017686e98a4889713dc1
alcubierre71 <81297415+alcubierre71@users.noreply.github.com>
2025-01-09 17:29:51 +0100

    Update config.js
    Client ID of the SPA - Simple Page App

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Q50FsV5ZJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5kkbicb54l6fukmfvkoq0d6f5e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1gpaa9834j1e2t8blqig00891f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
